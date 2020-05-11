4a2204285c9f7b870b9ee84a5a5d5c7b2cdc7dfe
Bryan Lo <lbryan250@gmail.com>
2020-05-11 10:50:39 -0700

    trying a different way to change the title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Welcome from './components/Welcome'
 import Discussion from './components/Discussion'
 import './App.css'
 
 function App() {
+
+    useEffect(() => {
+        document.title = "Bryan's Student Dashboard"
+    }, []);
+
     return (
         <div className='App'>
             <Welcome/>
