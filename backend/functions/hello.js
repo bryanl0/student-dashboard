bd50faf8da04d4c41465bb90623539c7105947d7
Bryan Lo <lbryan250@gmail.com>
2020-05-13 14:07:25 -0700

    trying to call netlify functions from frontend code

--- backend/functions/hello.js
+++ backend/functions/hello.js
@@ -1,9 +1,8 @@
 // test function for netlify
 
 exports.handler = function(event, context, callback) {
-    let response = {name: "Bryan"}
     callback(null, {
         statusCode: 200,
-        body: response
+        body: "Hello World"
     });
 };
